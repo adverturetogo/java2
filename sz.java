da31cb66a0666688c0ab40ff4b70ad21d0e16bd1
adverturetogo <2323100321@qq.com>
2023-03-07 18:05:29 +0800

    maybe

---
+++ sz.java
@@ -0,0 +1,10 @@
+public class sz 
+{
+    public static final double CM_PER_INCN=2.54;
+    public static void main (String[] args)
+    {
+        double paperWidth=8.5;
+        double paperHeight=11;
+        System.out.println("Paper size in centimeters:"+paperWidth*CM_PER_INCN+"by"+paperHeight*CM_PER_INCN);
+    }
+}
